19104c28816d69937f773e5ec050ea14d515d7fa
Ivan Laver <74679323+Ereyzer@users.noreply.github.com>
2021-04-08 22:47:46 +0300

    football-2

--- football.js
+++ football.js
@@ -2,42 +2,41 @@ const field = document.querySelector('#field')
 const ball = document.querySelector('#ball')
 const widthBallHalf = ball.width / 2;
 const heightBallHalf = ball.height / 2;
-console.log(widthBallHalf);
-console.log(heightBallHalf);
+// console.log(widthBallHalf);
+// console.log(heightBallHalf);
 const fieldSizeY = field.clientHeight;
 const fieldSizeX = field.clientWidth;
-console.dir(field);
-console.log(fieldSizeY);
-console.log(fieldSizeX);
+// console.dir(field);
+// console.log(fieldSizeY);
+// console.log(fieldSizeX);
 // const mouseMove = field.addEventListener('mousemove', (event) => console.table(event));
 const onMouseClick = field.addEventListener('click', (event) => coordinatesOfMouse(event));
 
-function coordinatesOfMouse(event) {
+function coordinatesOfMouse({offsetX, offsetY}) {
     let ballX = 0;
     let ballY = 0;
     // console.table(event);
-    console.log(event.offsetX);
-    console.log(event.offsetY);
-    if (event.offsetX < widthBallHalf) {
+    // console.log(event.offsetX);
+    // console.log(event.offsetY);
+    if (offsetX < widthBallHalf) {
         ballX = widthBallHalf;
-    }else if (event.offsetX > fieldSizeX - widthBallHalf) {
+    }else if (offsetX > fieldSizeX - widthBallHalf) {
         ballX = fieldSizeX - widthBallHalf;
     }else{
-        ballX = event.offsetX
+        ballX = offsetX
     };
-    if (event.offsetY < heightBallHalf) {
+    
+    if (offsetY < heightBallHalf) {
         ballY = heightBallHalf;
-    }else if (event.offsetY > fieldSizeY - heightBallHalf) {
+    }else if (offsetY > fieldSizeY - heightBallHalf) {
         ballY = fieldSizeY - heightBallHalf;
     }else{
-        ballY = event.offsetY;
-    }
-    // let ballX = event.offsetX < widthBallHalf || event.offsetX > fieldSizeX - widthBallHalf ? widthBallHalf : event.offsetX;
-    // let ballY = event.offsetY < heightBallHalf ? heightBallHalf : event.offsetY;
-    
+        ballY = offsetY;
+    };
+
     
     ball.setAttribute('style', `top: ${ballY}px;
                                  left: ${ballX}px;
-                                transform: translate(-50%, -50%)`)
+                                transform: translate(-50%, -50%)`);
     
 }
